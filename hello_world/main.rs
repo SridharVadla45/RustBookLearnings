d7f8693e353bd75798200dc68846800c9d43bf21
SridharVadla <vadlasreedhar123@gmail.com>
2024-08-28 18:40:35 +0530

    chapter 1 completed

---
+++ hello_world/main.rs
@@ -0,0 +1,7 @@
+fn main(){
+  printl("Hello Sridhar , Welcome to Rust programming language !")
+}
+
+
+// rustc filename.rs used to compile the rust source into machine code 
+// run filename.exe - runs the executable file 
